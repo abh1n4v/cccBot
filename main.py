439e5b6406373490aeb121d786968ba4c1761310
abh1n4v <abhinav70315@gmail.com>
2023-05-01 02:23:04 +0530

    added capture function

--- main.py
+++ main.py
@@ -46,13 +46,34 @@ def switchTab():
         keyboard.release(Key.tab)
     time.sleep(0.5)
 
+def capture():
+    pointers = {}
+    print('Hover your mouse on "CHECK ANSWER" button')
+    time.sleep(3)
+    pointers['check_answer'] = mouse.position
+
+    print('Hover your mouse on "NEXT" button')
+    time.sleep(3)
+    pointers['next'] = mouse.position
+
+    print('Hover your mouse on "SUBMIT CODE" button')
+    time.sleep(3)
+    pointers['submit'] = mouse.position
+
+    return (pointers)
+
+def mouse_reset(x,y):
+    mouse.move(x,y)
+    time.sleep(0.5)
+
 def main():
+    pointers = capture()
     n = int(input("Enter number of questions: "))
     for i in range(n):
         time.sleep(3)
-        setMousePoition(1177,1000)  #check answer
+        setMousePoition(pointers['check_answer'][0],pointers['check_answer'][1])  #check answer
         clickMouse()
-        setMousePoition(1177,650) #move up
+        mouse_reset(0,-270) #move up
         scrollMouse(0,-20)
         clickMouse()
         selectAll()
@@ -67,16 +88,17 @@ def main():
         clickMouse()
         selectAll()
         keyboard.type("/do")
-        setMousePoition(1643,1000) #submit
+        setMousePoition(pointers['submit'][0],pointers['submit'][1]) #submit
         clickMouse()
-        time.sleep(3)
-        setMousePoition(810, 1000) #next
+        time.sleep(5)
+        setMousePoition(pointers['next'][0],pointers['next'][1]) #next
         clickMouse()
+        time.sleep(0.5)
         keyboard.tap(Key.esc)
-        setMousePoition(923,649)
+        mouse_reset(0,-100)
+        time.sleep(1)
         clickMouse()
-        time.sleep(3)
+        keyboard.tap(Key.tab)
+        keyboard.tap(Key.enter)
 
 main()
-
-
